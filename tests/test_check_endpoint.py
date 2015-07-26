0fa23446e5c2a63b64fa3237df1172f515c35c6c
twirrim <paulgraydon@hotmail.com>
2015-07-26 08:12:03 -0700

    Initial tests
    Working through the code, these seem like a basic set of tests we wish to carry out.
    These have been deliberately created as "assert False" so that they all fail.  These will be fixed as we go along.

---
+++ tests/test_check_endpoint.py
@@ -0,0 +1,22 @@
+import pytest
+
+def test_get_connection():
+    assert False
+
+def test_verify_hostname_with_valid_hostname():
+    assert False
+
+def test_verify_hostname_with_valid_altname():
+    assert False
+
+def test_verify_hostname_with_invalid_hostname():
+    assert False
+
+def test_expiring_certificate_with_good_cert():
+    assert False
+
+def test_expiring_certificate_with_bad_cert():
+    assert false
+
+def test_send_email():
+    assert False
