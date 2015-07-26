f48554bcc5ac1161314592cb43ba65701d387289
twirrim <paulgraydon@hotmail.com>
2015-07-26 10:36:03 -0700

    Add fake connection class, PEP8 changes
    Also had a bad assert in there

--- tests/test_check_endpoint.py
+++ tests/test_check_endpoint.py
@@ -1,22 +1,45 @@
 import pytest
 
+# We're going to fake a connection for purposes of testing.
+# So far all we use is getpeercert method, so that's all we need to fake
+class fake_connection(object):
+    def __init__(self):
+        pass
+
+    def getpeercert(self):
+        cert_details = {'notAfter': 'Dec 31 00:00:00 2015 GMT',
+                        'subjectAltName': (('DNS', 'www.fake.com'),),
+                        'subject': ((('countryName', u'US'),),
+                                    (('stateOrProvinceName', u'Oregon'),),
+                                    (('localityName', u'Springfield'),),
+                                    (('organizationName', u'FakeCompany'),),
+                                    (('commonName', u'fake.com'),))}
+        return cert_details
+
+
 def test_get_connection():
     assert False
+
 
 def test_verify_hostname_with_valid_hostname():
     assert False
 
+
 def test_verify_hostname_with_valid_altname():
     assert False
+
 
 def test_verify_hostname_with_invalid_hostname():
     assert False
 
+
 def test_expiring_certificate_with_good_cert():
     assert False
 
+
 def test_expiring_certificate_with_bad_cert():
-    assert false
+    assert False
+
 
 def test_send_email():
     assert False
